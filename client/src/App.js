f804d8567642dea13a8f758f0cb15069c3470c5e
Nick <nick@panagakis.com>
2020-02-13 20:36:16 -0600

    added a basic map

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Register from "components/Register";
+import MapContainer from "components/MapContainer"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <MapContainer></MapContainer>
         <Register></Register>
       </header>
     </div>
