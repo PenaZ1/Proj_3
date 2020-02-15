ae74ea06ec8bf9d4e99ea2d2a47385a86a946a13
Nick <nick@panagakis.com>
2020-02-15 14:04:20 -0600

    Added files

---
+++ db/populateLocationSchema.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const Location = require('./Location');
+
+const populateSchema = () => {
+  const hospitals = JSON.parse(fs.readFileSync('./Hospitals_in_Mississippi.geojson')).features;
+  Object.values(hospitals).forEach((hospital) => {
+    Location.create({
+      name: hospital.properties.Name,
+      lat: hospital.geometry.coordinates[1],
+      long: hospital.geometry.coordinates[0],
+    });
+  });
+};
+
+module.exports = populateSchema;
