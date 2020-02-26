9cc5527936117e87f51e79caac98da60487e8786
Nick <nick@panagakis.com>
2020-02-25 19:33:06 -0600

    More updates to register

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import { AST_DefClass } from "terser";
 import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import "../styles/Register.css"
+import Hero from "./Hero"
 
 function Register(){
     const [email, setEmail] = useState('');
@@ -18,13 +20,16 @@ function Register(){
 
     return(
         <>
-            <h2>Register</h2>
-            <form>
-                Email: <input onChange={event => setEmail(event.target.value)}></input>
-                Password: <input onChange={event => setPassword(event.target.value)}></input>
-                Terms and Conditions: <input type="checkbox"></input> 
-                <button onClick={signUp}>Sign up</button>
-            </form>
+          <div id="Register">
+              <h2>Register</h2>
+              <form>
+                  Email: <br/><input onChange={event => setEmail(event.target.value)}></input> <br/>
+                  Password: <br/><input onChange={event => setPassword(event.target.value)}></input><br/>
+                  Terms and Conditions: <input type="checkbox"></input> <br/>
+                  <Button onClick={signUp}>Sign up</Button>
+              </form>
+          </div>
+          <Hero></Hero>
         </>
     );
 }
